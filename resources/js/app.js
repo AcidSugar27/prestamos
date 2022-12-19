446575148d3e146acbed3fa5093564e568426c37
AcidSugar27 <eliasamarante27@gmail.com>
2022-12-19 10:11:06 -0600

    Se agrego la libreria de Jquery npm i jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,5 +4,16 @@ import './bootstrap';
 
 // import './validation';
 
+/* ApexCharts
+ * url: https://apexcharts.com/
+ * cmd: npm i apexcharts
+ */
 import apex from 'apexcharts';
 window.ApexCharts = apex;
+
+/* jQuery
+ * url: https://jquery.com/
+ * cmd: npm i jquery
+ */
+import $ from 'jquery';
+window.$ = window.jQuery = $;
